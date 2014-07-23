b4c1549447ddcd8184997880c2617a041c3be9ea
Forest Gregg <fgregg@uchicago.edu>
2014-07-23 11:08:41 -0500

    Remove derived figures from enrolment

--- data/scripts/enrolment.sh
+++ data/scripts/enrolment.sh
@@ -1,5 +1,4 @@
-in2csv ../raw_data/enrollment/cps_2013-14\ high\ school\ tarantula_2lwg.xlsx | \
-tail -n +3 | \
-sed 's/=//g' | \
-sed 's/#DIV\/0!//g' > \
+in2csv ../raw_data/cps_2013-14\ high\ school\ tarantula_2lwg.xlsx | \
+csvcut -c 1-6 | \
+tail -n +3 > \
 ../processed_data/2013-2014_enrollment_high_school.csv
