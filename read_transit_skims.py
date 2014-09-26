781a8e3b05637f89ddb85f5192ca79540c8a74d6
Eric van Zanten <eric.vanzanten@gmail.com>
2014-09-26 09:25:17 -0500

    Updated import script.

--- read_transit_skims.py
+++ read_transit_skims.py
@@ -14,32 +14,41 @@ time_fields = {
 }
 
 if __name__ == "__main__":
-    for source_file in ['mf22.txt', 'mf23.txt', 'mf24.txt']:
-        fpath = join(data_dir, source_file)
-        time_col = time_fields[source_file]
-        fields = [
-            'source', 
-            'dest', 
-            time_col,
-        ]
-        table = SourceDest.__table__
-        conn = engine.connect()
-        with open(fpath, 'rU') as f:
-            [f.next() for i in range(4)]
-            for line in f:
-                parts = line.strip().split(' ')
-                source = parts[0]
-                destinations = [i.split(':') for i in parts[1:]]
-                vals = []
-                for dest in destinations:
-                    vals.append({k:v for k,v in zip(fields,(source, dest[0], dest[1],))})
-                ins = table.insert()
-                try:
-                    conn.execute(ins, vals)
-                except IntegrityError:
-                    for val in vals:
-                        u = {time_col: val[time_col]}
-                        upd = table.update().where(table.c.source == val['source'])\
-                            .where(table.c.dest == val['dest'])\
-                            .values(**u)
-                        conn.execute(upd)
+    import sys
+    try:
+        source_file = sys.argv[1]
+    except IndexError:
+        print 'You need to supply the name of a skim file'
+        sys.exit()
+    fpath = join(data_dir, source_file)
+    time_col = time_fields[source_file]
+    fields = [
+        'source', 
+        'dest', 
+        time_col,
+    ]
+    table = SourceDest.__table__
+    conn = engine.connect()
+    source_const = ''
+    with open(fpath, 'rU') as f:
+        [f.next() for i in range(4)]
+        for line in f:
+            parts = line.strip().split(' ')
+            source = parts[0]
+            if source != source_const:
+                source_const = source
+                print 'Loading zone %s (%s)' % (source, time_col)
+            destinations = [i.split(':') for i in parts[1:]]
+            vals = []
+            for dest in destinations:
+                vals.append({k:v for k,v in zip(fields,(source, dest[0], dest[1],))})
+            ins = table.insert()
+            try:
+                conn.execute(ins, vals)
+            except IntegrityError:
+                for val in vals:
+                    u = {time_col: val[time_col]}
+                    upd = table.update().where(table.c.source == val['source'])\
+                        .where(table.c.dest == val['dest'])\
+                        .values(**u)
+                    conn.execute(upd)
